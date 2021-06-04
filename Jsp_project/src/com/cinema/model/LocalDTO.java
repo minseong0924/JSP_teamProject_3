00cc89c187c2717c87d4800c8e061b6fa613ebea
rnfls <rnfls@LAPTOP-TS54VVTP>
2021-06-04 17:49:40 +0900

    20210604

---
+++ Jsp_project/src/com/cinema/model/LocalDTO.java
@@ -0,0 +1,19 @@
+package com.cinema.model;
+
+public class LocalDTO {
+	private int localcode;
+	private String localname;
+	
+	public int getLocalcode() {
+		return localcode;
+	}
+	public void setLocalcode(int localcode) {
+		this.localcode = localcode;
+	}
+	public String getLocalname() {
+		return localname;
+	}
+	public void setLocalname(String localname) {
+		this.localname = localname;
+	}
+}
