8c95ceef33245821d9bf35aabfffccae43011c6f
kmsol <kmsol@119.192.26.136>
2021-06-04 09:27:32 +0900

    ,,

--- Jsp_project/WebContent/sql/Member1.sql
+++ Jsp_project/WebContent/sql/Member1.sql
@@ -14,7 +14,7 @@ insert into member1 values('2', '1', '유관순', '010-1111-1112', default, defa
 insert into member1 values('3', '1', '세종대왕', '010-1111-1113', default, default, '1996-09-24', sysdate);
 insert into member1 values('4', '1', '광개토대왕', '010-1111-1114', default, default, '1996-09-24', sysdate);
 insert into member1 values('5', '1', '길민성', '010-1111-1115', default, default, '1996-09-24', sysdate);
-insert into member1 values('6', '1', '이상민', '010-1111-1116', default, default, '1996-09-24', sysdate);
+insert into member1 values('6', '1', '전상민', '010-1111-1116', default, default, '1996-09-24', sysdate);
 insert into member1 values('7', '1', '서혜빈', '010-1111-1117', default, default, '1996-09-24', sysdate);
 insert into member1 values('8', '1', '최범균', '010-1111-1118', default, default, '1996-09-24', sysdate);
 insert into member1 values('9', '1', '아몬드', '010-1111-1119', default, default, '1996-09-24', sysdate);
