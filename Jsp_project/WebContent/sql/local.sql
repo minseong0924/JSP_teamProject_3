4076e9dba6b459c3c28373fa62bd1581b48b15b2
rnfls <rnfls@LAPTOP-TS54VVTP>
2021-06-10 17:28:16 +0900

    20210610

--- Jsp_project/WebContent/sql/local.sql
+++ Jsp_project/WebContent/sql/local.sql
@@ -2,3 +2,7 @@ create table local(
     localcode number(5) primary key,
     localname varchar2(20) not null
 );
+
+insert into local values('1', '서울');
+insert into local values('2', '경기');
+insert into local values('3', '인천');
