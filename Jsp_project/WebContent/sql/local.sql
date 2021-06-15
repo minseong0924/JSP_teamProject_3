c3461cd2c0ad598c9dab7f0afd45bed3d8b36053
DESKTOP-3HJVSO7\sist <rhfqlsdkdl18@gmail.com>
2021-06-15 12:02:35 +0900

    1. 상영정보 삽입관련 로직 수정(최대 하루까지만 삽입 가능하도록 함) 2. 쿼리문 수정 및 더미데이터용 쿼리 추가

--- Jsp_project/WebContent/sql/local.sql
+++ Jsp_project/WebContent/sql/local.sql
@@ -10,7 +10,6 @@ insert into local values(10003,'인천');
 insert into local values(10004,'대전/충청/세종');
 insert into local values(10007,'부산/대구/경상');
 insert into local values(10010,'광주/전라');
-insert into local values(10011,'전라');
 insert into local values(10012,'강원');
 insert into local values(10013,'제주');
 
--- Jsp_project/WebContent/sql/movie.sql
+++ Jsp_project/WebContent/sql/movie.sql
@@ -1,17 +1,17 @@
 create table movie(
     movieCode number(5) primary key,
-    title_ko varchar2(30) not null,
-    title_en varchar2(30) not null,
-    poster varchar2(50) not null,
-    genre varchar2(20) not null,
-    director varchar2(30) not null,
-    actor varchar2(50) not null,
+    title_ko varchar2(1000) not null,
+    title_en varchar2(1000) not null,
+    poster varchar2(300) not null,
+    genre varchar2(300) not null,
+    director varchar2(300) not null,
+    actor varchar2(300) not null,
     summary varchar2(1500) not null,
     runningtime number(20) not null,
-    age varchar2(30) not null,
-    nation varchar2(20) not null,
+    age varchar2(300) not null,
+    nation varchar2(200) not null,
     opendate date not null,
-    mstate varchar2(20) not null,
-    mtype varchar2(20) not null
+    mstate varchar2(200) not null,
+    mtype varchar2(200) not null
 );
 
